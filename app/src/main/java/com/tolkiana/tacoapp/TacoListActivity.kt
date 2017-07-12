5f04eb58959d150181b9c703e26ccca30be8ab08
tolkiana <tolkiana@gmail.com>
2017-07-11 21:20:25 -0400

    Intial commit, creates home activity

---
+++ app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
@@ -0,0 +1,12 @@
+package com.tolkiana.tacoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TacoListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taco_list)
+    }
+}
