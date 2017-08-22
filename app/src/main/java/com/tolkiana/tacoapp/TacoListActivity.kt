56538f87d43dc1b7361325c44f30d7eed1781a8b
tolkiana <tolkiana@gmail.com>
2017-08-21 21:21:14 -0400

    Implements Product adapter in recycler view

--- app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
@@ -2,11 +2,22 @@ package com.tolkiana.tacoapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_taco_list.*
+import java.sql.Array
+import java.util.*
 
 class TacoListActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_taco_list)
+
+        val tacoList = Arrays.asList(Product("Pastor"), Product("Canasta"), Product("Asada"))
+        val productAdapter = ProductAdapter(tacoList)
+
+        tacoRecyclerView.layoutManager = LinearLayoutManager(this)
+        tacoRecyclerView.adapter = productAdapter
     }
 }
+
