90a2658a211c6f15e483be14d70925adb6aaadac
tolkiana <tolkiana@gmail.com>
2017-09-09 23:27:25 -0400

    Adds more properties to Product model

--- app/src/main/java/com/tolkiana/tacoapp/Product.kt
+++ app/src/main/java/com/tolkiana/tacoapp/Product.kt
@@ -6,4 +6,8 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
  * Created by tolkiana on 7/11/17.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class Product(val name: String)
+data class Product(val code: String,
+                   val name: String,
+                   val imageURL: String,
+                   val ingredients: List<String>,
+                   val preparation: List<String>)
