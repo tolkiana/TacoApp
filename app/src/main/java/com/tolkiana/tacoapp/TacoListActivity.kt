7fcbef81c3697af817ac288581f85a4f0d302f5f
tolkiana <tolkiana@gmail.com>
2017-09-17 16:53:58 -0400

    Creates a Singleton for the ImageLoader

--- app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
@@ -3,9 +3,7 @@ package com.tolkiana.tacoapp
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import com.android.volley.toolbox.ImageLoader
-import com.android.volley.toolbox.Volley
-import com.tolkiana.tacoapp.utilities.BitmapCache
+import com.tolkiana.tacoapp.utilities.ApplicationImageLoader
 import com.tolkiana.tacoapp.utilities.DataParser
 import kotlinx.android.synthetic.main.activity_taco_list.*
 
@@ -16,8 +14,7 @@ class TacoListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_taco_list)
 
         val tacoList = DataParser.parseArrayFromAsset<Product>(this.assets, "tacos.json")
-        val imageLoader = ImageLoader(Volley.newRequestQueue(this), BitmapCache())
-        val productAdapter = ProductAdapter(tacoList, imageLoader)
+        val productAdapter = ProductAdapter(tacoList, ApplicationImageLoader.getInstance(this))
 
         tacoRecyclerView.layoutManager = LinearLayoutManager(this)
         tacoRecyclerView.adapter = productAdapter
