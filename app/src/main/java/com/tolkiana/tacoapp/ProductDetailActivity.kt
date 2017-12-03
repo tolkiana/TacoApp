3b3600c4ad801094b2c3068b439cc1342aad1d27
tolkiana <tolkiana@gmail.com>
2017-12-03 18:36:20 -0500

    Creates an activity that will display product details

---
+++ app/src/main/java/com/tolkiana/tacoapp/ProductDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.tolkiana.tacoapp
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by tolkiana on 12/3/17.
+ */
+class ProductDetailActivity: AppCompatActivity {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
