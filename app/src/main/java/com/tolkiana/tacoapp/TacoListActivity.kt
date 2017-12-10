3c6e28940e680a419fb9db81490b807af6aca38a
tolkiana <tolkiana@gmail.com>
2017-12-10 18:56:37 -0500

    Implements OnItemClickListener which will open the product detail

--- app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/TacoListActivity.kt
@@ -1,13 +1,15 @@
 package com.tolkiana.tacoapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Looper
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import com.tolkiana.tacoapp.services.ProductsService
 import com.tolkiana.tacoapp.utilities.ApplicationImageLoader
 import kotlinx.android.synthetic.main.activity_taco_list.*
 
-class TacoListActivity : AppCompatActivity() {
+class TacoListActivity : AppCompatActivity(), ProductAdapter.OnItemClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,9 +18,15 @@ class TacoListActivity : AppCompatActivity() {
         val productService = ProductsService("https://afternoon-sea-72400.herokuapp.com")
         productService.getTacosList { tacoList ->
             val productAdapter = ProductAdapter(tacoList, ApplicationImageLoader.getInstance(this))
+            productAdapter.onItemClickListener = this
             tacoRecyclerView.layoutManager = LinearLayoutManager(this)
             tacoRecyclerView.adapter = productAdapter
         }
+    }
+
+    override fun onItemClick(product: Product) {
+        val intent = Intent(this, ProductDetailActivity::class.java)
+        startActivity(intent)
     }
 }
 
