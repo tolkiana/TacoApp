532da79a092cd95da18fd34997a18dbd4ce9f25b
Nelida Velazquez <nelida.velazquez@detroitlabs.com>
2017-12-12 16:50:31 -0500

    Receives the product, for now just print it

--- app/src/main/java/com/tolkiana/tacoapp/ProductDetailActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/ProductDetailActivity.kt
@@ -3,6 +3,7 @@ package com.tolkiana.tacoapp
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 
 /**
  * Created by tolkiana on 12/3/17.
@@ -12,5 +13,8 @@ class ProductDetailActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_product_detail)
+
+        val product = getIntent().getParcelableExtra<Product>(TacoListActivity.EXTRA_PRODUCT)
+        Log.d("Product", product.toString())
     }
 }
