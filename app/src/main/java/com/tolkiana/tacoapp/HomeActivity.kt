bc83c8a394c297686d06b99ec4115b67cb3fad2c
tolkiana <tolkiana@gmail.com>
2018-01-29 23:26:11 -0500

    Convert main menu to use cardViews

--- app/src/main/java/com/tolkiana/tacoapp/HomeActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/HomeActivity.kt
@@ -1,19 +1,13 @@
 package com.tolkiana.tacoapp
 
-import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_home.*
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-
-        famousTacosButton.setOnClickListener {
-            val intent = Intent(this, TacoListActivity::class.java)
-            startActivity(intent)
-        }
     }
 }
