230035127645c26e86395405ff178f18b2b0e57b
tolkiana <tolkiana@gmail.com>
2018-01-29 23:36:51 -0500

    Adds a click listener for the famous tacos the card view

--- app/src/main/java/com/tolkiana/tacoapp/HomeActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/HomeActivity.kt
@@ -1,13 +1,19 @@
 package com.tolkiana.tacoapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        famousTacosCardView.setOnClickListener {
+            val intent = Intent(this, TacoListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
