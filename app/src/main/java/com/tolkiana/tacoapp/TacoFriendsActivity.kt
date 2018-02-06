c612be2ea1ffef364730724662c1b4ecf0972eb9
tolkiana <tolkiana@gmail.com>
2018-02-05 21:08:40 -0500

    Creates activity for taco friends

---
+++ app/src/main/java/com/tolkiana/tacoapp/TacoFriendsActivity.kt
@@ -0,0 +1,15 @@
+package com.tolkiana.tacoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+
+class TacoFriendsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taco_friends)
+
+
+    }
+}
