9c0cfccc062c415eaa8691630ff1acd714165a25
Adrian Pena <rastadrian@gmail.com>
2018-03-01 23:05:43 -0500

    Updates fragment data persistence

--- app/src/main/java/com/tolkiana/tacoapp/TacoFriendFragment.kt
+++ app/src/main/java/com/tolkiana/tacoapp/TacoFriendFragment.kt
@@ -11,17 +11,20 @@ import com.tolkiana.tacoapp.services.ProductType
 import com.tolkiana.tacoapp.services.ProductsService
 import com.tolkiana.tacoapp.utilities.ApplicationImageLoader
 import kotlinx.android.synthetic.main.fragment_taco_friend.*
+import java.io.Serializable
 
 
 /**
  * A simple [Fragment] subclass.
  */
 class TacoFriendFragment : Fragment(), ProductAdapter.OnItemClickListener {
+
     companion object {
         private val ARG_PRODUCT_TYPE = "productType"
+        private val ARG_PRODUCT_LIST = "productList"
 
-        fun newInstance(productType: ProductType): TacoFriendFragment{
-            val arguments: Bundle = Bundle()
+        fun newInstance(productType: ProductType): TacoFriendFragment {
+            val arguments = Bundle()
             arguments.putSerializable(ARG_PRODUCT_TYPE, productType)
             val fragment = TacoFriendFragment()
             fragment.arguments = arguments
@@ -29,16 +32,33 @@ class TacoFriendFragment : Fragment(), ProductAdapter.OnItemClickListener {
         }
     }
 
+    private var productType: ProductType? = null
+    private var products: List<Product>? = null
+
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater?.inflate(R.layout.fragment_taco_friend, container, false)
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        (arguments.getSerializable(ARG_PRODUCT_TYPE) as ProductType)?.let {
-            fetchProductListForProductType(it)
+        arguments?.let {
+            this.productType = arguments.getSerializable(ARG_PRODUCT_TYPE) as ProductType
         }
+        if (savedInstanceState == null) { //This fragment is brand new
+            this.productType?.let {
+                fetchProductListForProductType(it)
+            }
+        } else { //This fragment is being recreated, maybe rotation happened.
+            this.products = savedInstanceState.getSerializable(ARG_PRODUCT_LIST) as List<Product>
+            this.products?.let {
+                displayProductList(it)
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putSerializable(ARG_PRODUCT_LIST, this.products as Serializable)
+        super.onSaveInstanceState(outState)
     }
 
     override fun onItemClick(product: Product) {
@@ -48,12 +68,17 @@ class TacoFriendFragment : Fragment(), ProductAdapter.OnItemClickListener {
     private fun fetchProductListForProductType(productType: ProductType) {
         tacoFriendProgressBar.visibility = View.VISIBLE
         val productService = ProductsService()
-        productService.fetchProductListForProductType(productType) { drinksList ->
-            val productAdapter = ProductAdapter(drinksList, ApplicationImageLoader.getInstance(context))
-            productAdapter.onItemClickListener = this
-            tacoFriendRecyclerView.layoutManager = LinearLayoutManager(context)
-            tacoFriendRecyclerView.adapter = productAdapter
-            tacoFriendProgressBar.visibility = View.GONE
+        productService.fetchProductListForProductType(productType) { products ->
+            this.products = products
+            displayProductList(products)
         }
+    }
+
+    private fun displayProductList(products: List<Product>) {
+        val productAdapter = ProductAdapter(products, ApplicationImageLoader.getInstance(context))
+        productAdapter.onItemClickListener = this
+        tacoFriendRecyclerView.layoutManager = LinearLayoutManager(context)
+        tacoFriendRecyclerView.adapter = productAdapter
+        tacoFriendProgressBar.visibility = View.GONE
     }
 }
