00e8276b07b849fb7d214408bef7e97641b7f33c
Adrian Pena <rastadrian@gmail.com>
2018-03-26 22:09:56 -0400

    Fixes some lint issues on Product and ProductService classes

--- app/src/main/java/com/tolkiana/tacoapp/services/ProductsService.kt
+++ app/src/main/java/com/tolkiana/tacoapp/services/ProductsService.kt
@@ -1,15 +1,15 @@
 package com.tolkiana.tacoapp.services
 
 import com.github.kittinunf.fuel.httpGet
-import com.tolkiana.tacoapp.Product
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.getAs
+import com.tolkiana.tacoapp.Product
 import com.tolkiana.tacoapp.utilities.DataParser
 
 /**
  * Created by tolkiana on 11/10/17.
  */
-class ProductsService() {
+class ProductsService {
 
     companion object {
         private const val TACO_SERVICE_URL = "https://afternoon-sea-72400.herokuapp.com"
@@ -17,12 +17,12 @@ class ProductsService() {
 
     fun fetchProductListForProductType(productType: ProductType, handler: (List<Product>) -> Unit) {
         val endpoint = TACO_SERVICE_URL + productType.path
-        endpoint.httpGet().responseString { request, response, result ->
+        endpoint.httpGet().responseString { _, _, result ->
             var tacoList = listOf<Product>()
             when (result) {
                 is Result.Success -> {
                     val json = result.getAs() ?: ""
-                    tacoList = DataParser.parseArrayFromJSON<Product>(json)
+                    tacoList = DataParser.parseArrayFromJSON(json)
                 }
             }
             handler(tacoList)
