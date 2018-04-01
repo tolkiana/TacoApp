f2817c66f88853b5f71f35db9e68a487ed300232
tolkiana <tolkiana@gmail.com>
2018-03-31 22:08:22 -0400

    Creates and links activity to display taco facts

---
+++ app/src/main/java/com/tolkiana/tacoapp/TacoFactsActivity.kt
@@ -0,0 +1,12 @@
+package com.tolkiana.tacoapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TacoFactsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_taco_facts)
+    }
+}
