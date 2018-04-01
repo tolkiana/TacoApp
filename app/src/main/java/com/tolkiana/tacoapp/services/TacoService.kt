0d8619786f2c80dd72793cbbc315aeec21cfb67d
tolkiana <tolkiana@gmail.com>
2018-03-31 22:03:31 -0400

    Adds function to fetch the taco facts

--- app/src/main/java/com/tolkiana/tacoapp/services/TacoService.kt
+++ app/src/main/java/com/tolkiana/tacoapp/services/TacoService.kt
@@ -4,6 +4,7 @@ import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
 import com.github.kittinunf.result.getAs
 import com.tolkiana.tacoapp.Product
+import com.tolkiana.tacoapp.TacoFact
 import com.tolkiana.tacoapp.utilities.DataParser
 
 /**
@@ -13,6 +14,20 @@ class TacoService {
 
     companion object {
         private const val TACO_SERVICE_URL = "https://afternoon-sea-72400.herokuapp.com"
+    }
+
+    fun fetchTacoFacts(handler: (List<TacoFact>) -> Unit) {
+        val endpoint = TACO_SERVICE_URL + "/facts"
+        endpoint.httpGet().responseString { _, _, result ->
+            var tacoFacts = listOf<TacoFact>()
+            when (result) {
+                is Result.Success -> {
+                    val json = result.getAs() ?: ""
+                    tacoFacts = DataParser.parseArrayFromJSON(json)
+                }
+            }
+            handler(tacoFacts)
+        }
     }
 
     fun fetchProductListForProductType(productType: ProductType, handler: (List<Product>) -> Unit) {
