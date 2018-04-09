d5fdb00b4e353228fcd6a4400d4c9d2cdf1a944b
tolkiana <tolkiana@gmail.com>
2018-04-08 20:55:38 -0400

    Add a progress hud for async call to get taco facts

--- app/src/main/java/com/tolkiana/tacoapp/TacoFactsActivity.kt
+++ app/src/main/java/com/tolkiana/tacoapp/TacoFactsActivity.kt
@@ -2,8 +2,10 @@ package com.tolkiana.tacoapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import com.tolkiana.tacoapp.services.TacoService
 import kotlinx.android.synthetic.main.activity_taco_facts.*
+import kotlinx.android.synthetic.main.activity_taco_list.*
 
 class TacoFactsActivity : AppCompatActivity() {
 
@@ -11,9 +13,11 @@ class TacoFactsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_taco_facts)
 
+        factsProgressBar.visibility = View.VISIBLE
         val productService = TacoService()
         productService.fetchTacoFacts { tacoFacts ->
             tacoFactsPager.adapter = TacoFactsPagerAdapter(tacoFacts, supportFragmentManager)
+            factsProgressBar.visibility = View.GONE
         }
     }
 }
